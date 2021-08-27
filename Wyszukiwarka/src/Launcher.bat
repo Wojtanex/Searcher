3816d1e5bac2f5670686c0db138b4dd40d1af4c1
Wojtanex <76729472+Wojtanex@users.noreply.github.com>
2021-08-27 12:38:22 +0200

    Add files via upload

---
+++ Wyszukiwarka/src/Launcher.bat
@@ -0,0 +1,21 @@
+@echo off
+Color 0A
+goto start
+
+:start
+echo Choose language:
+echo.
+echo 1. - polski
+echo 2. - english
+set /p wybierz=
+if %wybierz% == 1 goto PL
+if %wybierz% == 2 goto ENG
+
+
+:PL
+start WyszukiwarkaPL.bat
+exit
+
+:ENG
+start WyszukiwarkaENG.bat
+exit
